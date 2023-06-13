1296719c779653f6ad3da14c1b9d916301695d1e
babynaz <132637847+babynaz@users.noreply.github.com>
2023-06-13 08:49:32 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@First-LinkedList/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include "node.h"
+
+typedef int integer;
+
+int i;
+
+typedef node* nodePtr;
+ int main() {
+
+
+   
+   
+  //how to move temp to the next one?
+
+
+   
+  
+   
+}
